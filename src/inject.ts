e8f5aa4d3dd42666e45ddcf04d07fe0fba043654
Markus Mafz <65797058+headquarter8302@users.noreply.github.com>
2025-09-13 21:53:48 -0400

    ensure `result` isn't eliminated on build

--- src/inject.ts
+++ src/inject.ts
@@ -1,22 +1,23 @@
 import { type InjectReturnResult } from "./util";
 
+declare let result: Partial<InjectReturnResult>;
+
 const enum ElementSelectors {
 	ChannelName = "h1.dynamicTextViewModelH1 > span[role=\"text\"]",
 	ChannelVideoCount = "#page-header > yt-page-header-renderer > yt-page-header-view-model div.yt-page-header-view-model__page-header-headline yt-content-metadata-view-model > div:nth-child(3) > span:nth-child(3) > span",
 	// ChannelJoinDate = "#additional-info-container > table > tbody > tr:nth-child(5) > td:nth-child(2) > yt-attributed-string > span > span"
 }
 
-function InjectionScript() {
-	console.debug("[inject] TubiaBuddy successfully injected!");
+console.debug("[inject] TubiaBuddy successfully injected!");
 
-	console.debug("[inject] Getting page contents");
+console.debug("[inject] Getting page contents");
 
-	let result: Partial<InjectReturnResult> = {
-		channelDisplayName: (document.querySelector(ElementSelectors.ChannelName) as HTMLSpanElement)?.innerText,
-		// channelVidCount: (document.querySelector(ElementSelectors.ChannelVideoCount) as HTMLSpanElement)?.innerText.split(" ")[0]!,
-		// channelJoinDate: (document.querySelector(ElementSelectors.ChannelJoinDate) as HTMLSpanElement)?.innerText.split("Joined ")[1]
-	};
-	return result;
-}
+result = {
+	channelDisplayName: (document.querySelector(ElementSelectors.ChannelName) as HTMLSpanElement)?.innerText,
+	// channelVidCount: (document.querySelector(ElementSelectors.ChannelVideoCount) as HTMLSpanElement)?.innerText.split(" ")[0]!,
+	// channelJoinDate: (document.querySelector(ElementSelectors.ChannelJoinDate) as HTMLSpanElement)?.innerText.split("Joined ")[1]
+};
 
-InjectionScript();
+console.debug(JSON.stringify(result));
+
+result;
